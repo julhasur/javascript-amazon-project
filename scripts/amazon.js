49dd25ab73d913751fc6b666c574f4dad9189919
julhasur <shuvochowdhury14@gmail.com>
2024-05-07 15:54:42 -0400

    format currency shared function has been created and fixed choosing radio button with unique product id has been created.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 
 import {cart, addtocart} from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 
  let productsHTML='';
@@ -26,7 +27,7 @@ import { products } from "../data/products.js";
 
             <div class="product-price">
             
-                $${(product.priceCents/100).toFixed(2)}
+                $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
