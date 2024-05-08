9f1746f3da4841403a9b37118c74b98b259518f2
julhasur <shuvochowdhury14@gmail.com>
2024-05-08 14:19:44 -0400

    save data and generate html for delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,117 +1,127 @@
-import { cart } from "../data/cart.js";
-import {products} from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
-import { removeFromCart } from "../data/cart.js";
-import  dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-//calling dayjs external js file
-const today=dayjs();
-//date js takes two param 
-const deliveryDate=today.add(7,'days');
-console.log(deliveryDate);
-// use the documentation to show the format 
-console.log(deliveryDate.format('dddd,MMM D'))
-let cartSummeryHTML=''
-cart.forEach((cartItem)=>{ 
-    const productId=cartItem.productId;
-
-    let matchingProduct;
-    products.forEach((product)=>{
-        if(product.id===productId){
-            matchingProduct=product;  
-        } ;
-    });
+import {cart, removeFromCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliveryOptions } from '../data/deliveryOptions.js';
 
 
+let cartSummaryHTML = '';
 
-cartSummeryHTML+=`
-<div class="cart-item-container 
-js-cart-item-container-${matchingProduct.id}">
-<div class="delivery-date">
-  Delivery date: Tuesday, June 21
-</div>
+cart.forEach((cartItem) => {
+  const productId = cartItem.productId;
 
-<div class="cart-item-details-grid">
-  <img class="product-image"
-    src="${matchingProduct.image   }">
+  let matchingProduct;
 
-  <div class="cart-item-details">
-    <div class="${matchingProduct.name}">
-      Black and Gray Athletic Cotton Socks - 6 Pairs
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+
+  const deliveryOptionId=cartItem.deliveryOptionId;
+  let deliveryOption;
+  deliveryOptions.forEach((option)=>{
+  if(option.id===deliveryOptionId) {
+    deliveryOption=option;
+  }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  const dateString = deliveryDate.format("dddd,MMMM D");
+
+
+  
+  cartSummaryHTML += `
+    <div class="cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+      Delivery date: ${dateString}
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          ${deliveryOptionHTML(matchingProduct,cartItem)}
+        </div>
+      </div>
     </div>
-    <div class="product-price">
-     $${formatCurrency(matchingProduct.priceCents)}
-    </div>
-    <div class="product-quantity">
-      <span>
-        Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-      </span>
-      <span class="update-quantity-link link-primary">
-        Update
-      </span>
-      <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-        Delete
-      </span>
-    </div>
-  </div>
+  `;
+});
 
-  <div class="delivery-options">
-    <div class="delivery-options-title">
-      Choose a delivery option:
-    </div>
-    <div class="delivery-option">
-      <input type="radio" checked
+//creating html function for delivery options
+
+function deliveryOptionHTML(matchingProduct,cartItem) {
+  let html='';
+
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+    const dateString = deliveryDate.format("dddd,MMMM D");
+    const priceString=deliveryOption.priceCents===0 ?'FREE' :`$${formatCurrency(deliveryOption.priceCents)} -`;
+
+  const ischecked=deliveryOption.id===cartItem.deliveryOptionId;
+
+  html+=
+    ` <div class="delivery-option">
+      <input type="radio"
+      ${ischecked ? 'checked':''}
         class="delivery-option-input"
         name="delivery-option-${matchingProduct.id}">
       <div>
         <div class="delivery-option-date">
-          Tuesday, June 21
+        ${dateString}
         </div>
         <div class="delivery-option-price">
-          FREE Shipping
+        ${priceString} Shipping
         </div>
       </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id }">
-      <div>
-        <div class="delivery-option-date">
-          Wednesday, June 15
-        </div>
-        <div class="delivery-option-price">
-          $4.99 - Shipping
-        </div>
-      </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Monday, June 13
-        </div>
-        <div class="delivery-option-price">
-          $9.99 - Shipping
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-`;
-});
-document.querySelector('.js-order-summary').innerHTML=cartSummeryHTML;
-//making delte button responsive
-document.querySelectorAll('.js-delete-link').forEach((link)=>{
-    link.addEventListener('click',()=>{
-    const productId=link.dataset.productId;
-   removeFromCart(productId);
+    </div>`;
+  });
+  return html;
 
-  const container= document.querySelector(
-    `.js-cart-item-container-${productId}`
-   )
-  container.remove()
+}
+
+
+
+document.querySelector('.js-order-summary')
+  .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
-});
+  });
