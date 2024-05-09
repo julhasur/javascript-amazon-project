36f02dccc19d28bd21c928fdb7fdbcb854e074f4
julhasur <shuvochowdhury14@gmail.com>
2024-05-09 05:02:33 -0400

    checkout page rerender to update and regernate html

--- data/cart.js
+++ data/cart.js
@@ -1,57 +1,67 @@
-export  let cart=//GETTING THE ITEMS FROM LOCAL STORAGE and converting the saved string back to an array
-JSON.parse(localStorage.getItem('cart'))
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if(!cart){
-    cart=[{
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2,
-        deliveryOptionId:'1'
-    },{
-        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1,
-        deliveryOptionId:'2'
-    }
-    ];
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
 }
 
-//creating a local storage for the cart
-function saveToStorage(){
-    //converting data to string 
-    localStorage.setItem('cart',JSON.stringify(cart))
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
-export function addtocart(productId){
 
-    //let us loop through the cart so the button knows what and how to add to the cart and if the item is already in the cart and what action to do if it is , 
-    let matchingItem;
-   
-        cart.forEach((cartItem)=>{
-            if(productId===cartItem.productId){
-                matchingItem=cartItem;
-            }
-        });
-   
-        //we can just write matching here bcz its a truthy item
-        if(matchingItem){
-            matchingItem.quantity+=1
-        }else{
-            cart.push({
-                productId:productId,
-                quantity:1,
-                deliveryOptionId:'1'
-             });
-           }
-           saveToStorage();
-       
-    };
+export function addToCart(productId) {
+  let matchingItem;
 
-   export function removeFromCart(productId){
-        const newCart=[];
-
-        cart.forEach((cartItem)=> {
-            if (cartItem.productId !==productId){
-              newCart.push(cartItem);  
-            }
-        });
-        cart=newCart;
-        saveToStorage();
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
